45cdb5d0e853d1afdc60ad00289e16668cea96fd
Ross Culliton <1688ross@gmail.com>
2026-01-15 14:00:43 -0500

    Add ReciteMe client key

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -71,6 +71,14 @@ async function onRenderHtml(pageContext: { Page?: () => JSX.Element }) {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        ${dangerouslySkipEscape(`<script>
+        var serviceUrl = "//api.reciteme.com/asset/js?key=";
+        var serviceKey = "962e0b59b944e388d7d38c22413636845a6f8fd8";
+        var options = {};  // Options can be added as needed
+        var autoLoad = false;
+        var enableFragment = "#reciteEnable";
+        var loaded = [], frag = !1; window.location.hash === enableFragment && (frag = !0); function loadScript(c, b) { var a = document.createElement("script"); a.type = "text/javascript"; a.readyState ? a.onreadystatechange = function () { if ("loaded" == a.readyState || "complete" == a.readyState) a.onreadystatechange = null, void 0 != b && b() } : void 0 != b && (a.onload = function () { b() }); a.src = c; document.getElementsByTagName("head")[0].appendChild(a) } function _rc(c) { c += "="; for (var b = document.cookie.split(";"), a = 0; a < b.length; a++) { for (var d = b[a]; " " == d.charAt(0);)d = d.substring(1, d.length); if (0 == d.indexOf(c)) return d.substring(c.length, d.length) } return null } function loadService(c) { for (var b = serviceUrl + serviceKey, a = 0; a < loaded.length; a++)if (loaded[a] == b) return; loaded.push(b); loadScript(serviceUrl + serviceKey, function () { "function" === typeof _reciteLoaded && _reciteLoaded(); "function" == typeof c && c(); Recite.load(options); Recite.Event.subscribe("Recite:load", function () { Recite.enable() }) }) } "true" == _rc("Recite.Persist") && loadService(); if (autoLoad && "false" != _rc("Recite.Persist") || frag) document.addEventListener ? document.addEventListener("DOMContentLoaded", function (c) { loadService() }) : loadService();
+        </script>`)}
         <script src="https://cdn.givechariot.com/components/chariot-components.umd.js"></script>
         ${dangerouslySkipEscape((helmetContext as any).helmet?.title?.toString?.() || "")}
         ${dangerouslySkipEscape((helmetContext as any).helmet?.meta?.toString?.() || "")}
