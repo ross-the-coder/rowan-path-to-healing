2fa8f3e82522df2203971cb43a6ffd3bb5116792
Ross Culliton <1688ross@gmail.com>
2026-01-15 02:05:37 -0500

    Change git to GitHub

--- src/components/PillarsSection.tsx
+++ src/components/PillarsSection.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Shield, Phone, Heart, ChevronDown } from "lucide-react";
 import { Link } from "react-router-dom";
